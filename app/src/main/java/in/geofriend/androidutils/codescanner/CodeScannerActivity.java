91c859aa501652537365e87991e0ba2d96b634f3
Vivek <vkvashistha@gmail.com>
2023-04-15 22:29:21 +0530

    BarcodeScanner fixed

--- app/src/main/java/in/geofriend/androidutils/codescanner/CodeScannerActivity.java
+++ app/src/main/java/in/geofriend/androidutils/codescanner/CodeScannerActivity.java
@@ -3,11 +3,10 @@ package in.geofriend.androidutils.codescanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 import in.geofriend.androidutils.R;
-import in.geofriend.barcodescanner.BarcodeScanner;
+import in.geofriend.barcodescanner.v2.BarcodeScanner;
 
 public class CodeScannerActivity extends AppCompatActivity {
 
@@ -15,21 +14,15 @@ public class CodeScannerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code_scanner);
-        findViewById(R.id.btn_scan).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BarcodeScanner scanner = new BarcodeScanner(CodeScannerActivity.this);
-                scanner.scan(new BarcodeScanner.BarcodeScanResultCallback() {
-                    @Override
-                    public void onBarcodeScanCompleted(boolean success, String result) {
-                        if(success) {
-                            ((TextView) findViewById(R.id.tv_scanned_code)).setText(result);
-                        } else {
-                            ((TextView) findViewById(R.id.tv_scanned_code)).setText("Error");
-                        }
-                    }
-                });
-            }
+        BarcodeScanner scanner = new BarcodeScanner(CodeScannerActivity.this);
+        findViewById(R.id.btn_scan).setOnClickListener(v -> {
+            scanner.scan((success, result) -> {
+                if(success) {
+                    ((TextView) findViewById(R.id.tv_scanned_code)).setText(result);
+                } else {
+                    ((TextView) findViewById(R.id.tv_scanned_code)).setText("Error");
+                }
+            });
         });
     }
 }
